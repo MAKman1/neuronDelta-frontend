368c830113174be6709f5c3d9a14eb0b35c960e5
daniyal-khalil <daniyal.khalil@ug.bilkent.edu.tr>
2020-12-18 23:37:51 +0300

    Commit

---
+++ src/components/Manager/Headers/ProfileHeader.js
@@ -0,0 +1,32 @@
+/*! Developed by Alinon */
+import React from "react";
+
+// reactstrap components
+import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
+
+class DashboardHeader extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
+          <Container fluid>
+            <div className="header-body">
+                <Row>
+					<h1 className="text-white" style={{paddingLeft: 15, paddingBottom: 50}}>Jacobs & Sons</h1>
+                </Row>
+              {/* Card stats */}
+              <Row>
+
+               
+                
+                
+              </Row>
+            </div>
+          </Container>
+        </div>
+      </>
+    );
+  }
+}
+
+export default DashboardHeader;
